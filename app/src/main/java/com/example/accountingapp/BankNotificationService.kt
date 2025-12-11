ea5896ea8d40c3b4b6758dae15cb012dbbe71cc2
Pham Sy Phu <phamsyphu0912@gmail.com>
2025-12-11 11:14:17 +0700

    Lưu vào CSDL

--- app/src/main/java/com/example/accountingapp/BankNotificationService.kt
+++ app/src/main/java/com/example/accountingapp/BankNotificationService.kt
@@ -2,12 +2,35 @@ package com.example.accountingapp
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import com.example.accountingapp.Database.AppDatabase
+import com.example.accountingapp.Database.TransactionEntity
+import com.example.accountingapp.Database.TransactionRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class BankNotificationService: NotificationListenerService() {
+    private val repo by lazy {
+        val db = AppDatabase.getDatabase(this)
+        TransactionRepo(db.transactionDao())
+    }
+
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
         if (!Helper.isBankNotification(sbn)) return
+        val (title, text) = Helper.extractNotificationText(sbn)
+        val amount = Helper.parseAmount(text)
 
-        //Xử lý khi nhận được thông báo
+        val newTransaction = TransactionEntity(
+            fromUser = title,
+            content = text,
+            time = System.currentTimeMillis(),
+            amount = amount.toDouble()
+        )
+        //Do phải được gọi bằng hàm suspend nhưng ở đây k có nên phải thông qua CoroutineScope
+        CoroutineScope(Dispatchers.IO).launch{
+            repo.insert(newTransaction)
+        }
     }
 }
